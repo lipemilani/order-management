c3671f7572c5fd702cee829c7dafe7ec313742ae
Felipe Milani <flpmilani@hotmail.com>
2024-07-23 14:29:43 -0300

    Added password to userSeed. Readme

--- start.sh
+++ start.sh
@@ -5,6 +5,13 @@ if [ ! -f ".env" ]
     cp .env.example .env
 fi
 
+if [ ! -f "storage/oauth-private.key" ]
+  then
+    docker compose exec php php artisan passport:keys
+    docker compose exec php sh -c "printf '\n\n' | php artisan passport:client --password"
+fi
+
+
 composer install
 
 php artisan serve --host=0.0.0.0 --port=8000
